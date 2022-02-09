cdf3ffcade9ab94b728f1d852b5c0cfb2f1b8ace
drewfeld216 <dfeld216@gmail.com>
2022-02-09 12:28:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ app.config.from_mapping(
 
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
 # ensure the instance folder exists
 try:
